d20a8bf41297ea33d4fbea3366429f29a00a0009
bhnprksh222 <bhnprksh222@gmail.com>
2020-04-22 00:16:10 +0530

    implemented module pattern!

---
+++ app.js
@@ -0,0 +1,32 @@
+var budgetController = (function() {
+
+    var x = 23;
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+
+var UIcontroller = (function() {
+
+    //code
+
+})();
+
+
+var controller = (function(budgetCtrl, UIctrl) {
+
+    var z = budgetCtrl.publicTest(5);
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+})(budgetController, UIcontroller);
