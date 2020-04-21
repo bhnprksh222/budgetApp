f69b2cde491eaea3381af89a7a6dd90ea45fd223
bhnprksh222 <bhnprksh222@gmail.com>
2020-04-22 00:47:46 +0530

    added 'addEventListener' for 'add__btn'

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    //code
 
 })();
 
@@ -23,10 +15,24 @@ var UIcontroller = (function() {
 
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem  = function(){
+         // 1. Get the field input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+        console.log("check");
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
 })(budgetController, UIcontroller);
+
